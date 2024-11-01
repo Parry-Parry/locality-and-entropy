a3ee20b6fc8838c0ead79ec4c3533c3f27b1c2ad
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-11-01 12:46:42 +0000

    es

--- scripts/make_data.sh
+++ scripts/make_data.sh
@@ -1,16 +1,16 @@
-OUTPUT_DIR = 'sigir25-implicit-diff/data'
-CHECKPOINT = 'sigir25-implicit-diff/checkpoints/crossencoder'
-DATASET = "msmarco-passage/train/triples-small"
+OUTPUT_DIR='sigir25-implicit-diff/data'
+CHECKPOINT='sigir25-implicit-diff/checkpoints/crossencoder'
+DATASET="msmarco-passage/train/triples-small"
 # Create output directory
 mkdir -p $OUTPUT_DIR
 
 # Dump triples to file
-python -m implicit.data_processing.get_triples \
+python sigir25-implicit-diff/srcs/implicit/data_processing/get_triples.py \
     --dataset $DATASET \
     --out_file "${OUTPUT_DIR}/triples.tsv.gz"
 
 # Mine negatives
-python -m implicit.data_processing.mine_negatives \
+python sigir25-implicit-diff/srcs/implicit/data_processing/mine_negatives.py \
     --file "${OUTPUT_DIR}/triples.tsv.gz" \
     --output_dir $OUTPUT_DIR \
     --model_name_or_path $CHECKPOINT \
