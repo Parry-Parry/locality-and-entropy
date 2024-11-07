027a44ac816a7b62ff5e7e5313e94fc706c8ead5
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-11-07 14:04:51 +0000

    all fixed

---
+++ scripts/train_cross_bm25.sh
@@ -0,0 +1,38 @@
+MODEL_NAME="google/electra-base-discriminator"
+OUTPUT_DIR="checkpoints"
+WANDB_PROJECT="implicit-distillation"
+WARMUP_RATIO=0.1
+LR=5e-5
+FP16=true
+SAVE_LIMIT=1
+LOSS='lce'
+GROUP_SIZE=$1
+TRIPLE_FILE="data/bm25.${GROUP_SIZE}.jsonl.gz"
+BATCH_SIZE=$2
+GRAD_ACCUM=$3
+# optional teacher file is now last argument
+TEACHER_FILE=$4
+
+# Build base command
+CMD="python -m implicit.train \
+--model_name_or_path $MODEL_NAME \
+--output_dir $OUTPUT_DIR \
+--wandb_project $WANDB_PROJECT \
+--warmup_ratio $WARMUP_RATIO \
+--learning_rate $LR \
+--save_total_limit $SAVE_LIMIT \
+--loss_fn $LOSS \
+--training_dataset_file $TRIPLE_FILE \
+--group_size $GROUP_SIZE \
+--per_device_batch_size $BATCH_SIZE \
+--gradient_accumulation_steps $GRAD_ACCUM \
+--fp16 \
+--cat"
+
+# Add teacher file argument only if it's defined
+if [ ! -z "$TEACHER_FILE" ]; then
+    CMD="$CMD --teacher_file $TEACHER_FILE"
+fi
+
+# Execute the command
+eval $CMD
--- src/implicit/train_cat.py
+++ src/implicit/train_cat.py
@@ -16,7 +16,7 @@ def main():
     
     formatted_model_name = model_args.model_name_or_path.replace('/', '-')
     distilled = "distilled" if data_args.teacher_file is not None else "first"
-    training_args.output_dir = training_args.output_dir + f'/dot-{formatted_model_name}-{training_args.loss_fn.name}-{training_args.group_size}-{distilled}'
+    training_args.output_dir = training_args.output_dir + f'/cat-{formatted_model_name}-{training_args.loss_fn.name}-{training_args.group_size}-{distilled}'
 
     model = Cat.from_pretrained(model_args.model_name_or_path)
 
