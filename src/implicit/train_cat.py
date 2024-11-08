c9806a7ce9f40358eb4b376bd1750a7aabc7be74
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-11-08 14:31:48 +0000

    all fixed

--- src/implicit/train_cat.py
+++ src/implicit/train_cat.py
@@ -21,7 +21,7 @@ def main():
     training_args.output_dir = training_args.output_dir + f'/cat-{formatted_model_name}-{training_args.loss_fn.name}-{training_args.group_size}-{distilled}'
     model = Cat.from_pretrained(model_args.model_name_or_path)
 
-    dataset = TrainingDataset(data_args.training_data, corpus=data_args.ir_dataset, no_positive=data_args.no_positive)
+    dataset = TrainingDataset(data_args.training_dataset_file, corpus=data_args.ir_dataset, no_positive=data_args.no_positive)
     collate_fn = CatDataCollator(model.tokenizer)
 
     opt = AdamW(model.parameters(), lr=training_args.learning_rate)
