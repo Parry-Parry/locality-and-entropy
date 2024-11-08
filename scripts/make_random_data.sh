a71cdb59de10002f7f7295e279afcd6f32a7e97e
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-11-08 12:00:57 +0000

    make effectively random

---
+++ scripts/make_random_data.sh
@@ -0,0 +1,18 @@
+OUTPUT_DIR='sigir25-implicit-diff/data'
+CHECKPOINT='sigir25-implicit-diff/checkpoints/crossencoder'
+DATASET="msmarco-passage/train/triples-small"
+# Create output directory
+mkdir -p $OUTPUT_DIR
+
+# Dump triples to file
+python sigir25-implicit-diff/src/implicit/data_processing/get_triples.py \
+    --dataset $DATASET \
+    --out_file "${OUTPUT_DIR}/triples.jsonl.gz"
+
+# Mine negatives
+python sigir25-implicit-diff/src/implicit/data_processing/mine_negatives.py \
+    --file "${OUTPUT_DIR}/triples.jsonl.gz" \
+    --dataset $DATASET \
+    --depth 10000 \
+    --out_dir $OUTPUT_DIR \
+    --model_name_or_path $CHECKPOINT 
