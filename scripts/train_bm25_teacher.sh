30345f542f7c6cdeea6382a8e82c177e5f2dbb12
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-11-10 12:34:56 +0000

    add simple script

---
+++ scripts/train_bm25_teacher.sh
@@ -0,0 +1,45 @@
+MODEL_NAME="google/electra-base-discriminator"
+OUTPUT_DIR="checkpoints"
+WANDB_PROJECT="implicit-distillation"
+WARMUP_RATIO=0.1
+LR=5e-5
+FP16=true
+SAVE_LIMIT=1
+LOSS='lce'
+GROUP_SIZE=$1
+TRIPLE_FILE="data/bm25.${GROUP_SIZE}.jsonl"
+BATCH_SIZE=$2
+GRAD_ACCUM=$3
+MAX_STEPS=$4
+# optional teacher file is now last argument
+TEACHER_FILE=$5
+
+# Build base command
+CMD="python -m implicit.train_cat \
+--model_name_or_path $MODEL_NAME \
+--output_dir $OUTPUT_DIR \
+--wandb_project $WANDB_PROJECT \
+--warmup_ratio $WARMUP_RATIO \
+--learning_rate $LR \
+--save_total_limit $SAVE_LIMIT \
+--loss_fn $LOSS \
+--training_dataset_file $TRIPLE_FILE \
+--group_size $GROUP_SIZE \
+--per_device_train_batch_size $BATCH_SIZE \
+--gradient_accumulation_steps $GRAD_ACCUM \
+--ir_dataset "msmarco-passage/train/triples-small" \
+--fp16 t
+--report_to wandb"
+
+# Add max steps argument only if it's defined
+if [ ! -z "$MAX_STEPS" ]; then
+    CMD="$CMD --max_steps $MAX_STEPS"
+fi
+
+# Add teacher file argument only if it's defined
+if [ ! -z "$TEACHER_FILE" ]; then
+    CMD="$CMD --teacher_file $TEACHER_FILE"
+fi
+
+# Execute the command
+eval $CMD
