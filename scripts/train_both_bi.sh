5ff987a143eb524b2c1a0e1966aed1f7e879ab82
“Andrew <a.parry.1@research.gla.ac.uk>
2024-11-20 11:38:37 +0000

    fixed

--- scripts/train_both_bi.sh
+++ scripts/train_both_bi.sh
@@ -5,9 +5,21 @@ GRAD_ACCUM=$4
 CROSS_TEACHER_FILE="data/crossencoder.scores.json.gz"
 BM25_TEACHER_FILE="data/bm25.crossencoder.scores.json.gz"
 
-CMD = f"./scripts/train_bi_bm25 {LOSS} {GROUP_SIZE} {BATCH_SIZE} {GRAD_ACCUM} {BM25_TEACHER_FILE}"
+CMD="./scripts/train_bi_bm25 $LOSS $GROUP_SIZE $BATCH_SIZE $GRAD_ACCUM"
+
+# Check if LOSS is "lce"
+if [[ "$LOSS" != "lce" ]]; then
+    CMD="$CMD $BM25_TEACHER_FILE"
+fi
+
 eval $CMD
 
-CMD = f"./scripts/train_bi_teacher {LOSS} {GROUP_SIZE} {BATCH_SIZE} {GRAD_ACCUM} {CROSS_TEACHER_FILE}"
+CMD="./scripts/train_bi_teacher $LOSS $GROUP_SIZE $BATCH_SIZE $GRAD_ACCUM"
+
+# Check if LOSS is "lce"
+if [[ "$LOSS" != "lce" ]]; then
+    CMD="$CMD $CROSS_TEACHER_FILE"
+fi
+
 eval $CMD
 
--- scripts/train_both_cross.sh
+++ scripts/train_both_cross.sh
@@ -5,8 +5,20 @@ GRAD_ACCUM=$4
 CROSS_TEACHER_FILE="data/crossencoder.scores.json.gz"
 BM25_TEACHER_FILE="data/bm25.crossencoder.scores.json.gz"
 
-CMD="./scripts/train_cross_bm25 $LOSS $GROUP_SIZE $BATCH_SIZE $GRAD_ACCUM $CROSS_TEACHER_FILE"
+CMD="./scripts/train_cross_bm25 $LOSS $GROUP_SIZE $BATCH_SIZE $GRAD_ACCUM"
+
+# Check if LOSS is "lce"
+if [[ "$LOSS" != "lce" ]]; then
+    CMD="$CMD $BM25_TEACHER_FILE"
+fi
+
 eval $CMD
 
-CMD="./scripts/train_cross_teacher $LOSS $GROUP_SIZE $BATCH_SIZE $GRAD_ACCUM $CROSS_TEACHER_FILE"
+CMD="./scripts/train_cross_teacher $LOSS $GROUP_SIZE $BATCH_SIZE $GRAD_ACCUM"
+
+# Check if LOSS is "lce"
+if [[ "$LOSS" != "lce" ]]; then
+    CMD="$CMD $CROSS_TEACHER_FILE"
+fi
+
 eval $CMD
