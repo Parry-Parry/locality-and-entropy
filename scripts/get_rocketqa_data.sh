3bc216b6b9d11385ddd54c8b40adf45f6461f000
“Andrew <a.parry.1@research.gla.ac.uk>
2024-11-27 17:24:59 +0000

    minor fixes

--- scripts/get_rocketqa_data.sh
+++ scripts/get_rocketqa_data.sh
@@ -1,7 +1,6 @@
 wget -nv --no-check-certificate https://rocketqa.bj.bcebos.com/V1/data_train.tar.gz
-tar -zxf data_train.tar.gz
+tar -zxf data_train.tar.gz --no-same-owner
 rm -rf data_train.tar.gz
 
-for fold in marco nq;do
-    cat data_train/${fold}_de1_denoise.tsv data_train/${fold}_unlabel_de2_denoise.tsv > data_train/${fold}_merge_de2_denoise.tsv 
-done
+fold=marco
+cat data_train/${fold}_de1_denoise.tsv data_train/${fold}_unlabel_de2_denoise.tsv > data_train/${fold}_merge_de2_denoise.tsv 
