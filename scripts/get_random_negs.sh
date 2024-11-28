89adb4072a848ff6878728af1984df0bd6e495a0
“Andrew <a.parry.1@research.gla.ac.uk>
2024-11-28 10:36:17 +0000

    add random negs

---
+++ scripts/get_random_negs.sh
@@ -0,0 +1,12 @@
+OUTPUT_DIR='sigir25-implicit-diff/data'
+CHECKPOINT='sigir25-implicit-diff/checkpoints/crossencoder'
+DATASET="msmarco-passage/train/triples-small"
+# Create output directory
+mkdir -p $OUTPUT_DIR
+
+# Mine negatives
+python sigir25-implicit-diff/src/implicit/data_processing/mine_random_negatives.py \
+    --file "${OUTPUT_DIR}/triples.jsonl.gz" \
+    --dataset $DATASET \
+    --out_dir $OUTPUT_DIR \
+    --model_name_or_path $CHECKPOINT 
