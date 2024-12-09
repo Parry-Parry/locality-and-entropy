0fc7e35fdbafa3166e99e95a82e5fe9eaac18bc6
“Andrew <a.parry.1@research.gla.ac.uk>
2024-12-09 10:38:33 +0000

    silly

--- scripts/inventory.sh
+++ scripts/inventory.sh
@@ -2,5 +2,5 @@ DIRECTORY='checkpoints'
 OUTPUT='data/inventory.csv'
 
 python -m implicit.data_processing.get_inventory \
-    --directory $DIRECTORY
+    --directory $DIRECTORY \
     --output $OUTPUT
