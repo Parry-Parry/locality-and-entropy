f2936b2fcecc4eb32e3e0993791ec3b1adc5491e
“Andrew <a.parry.1@research.gla.ac.uk>
2025-03-25 18:07:36 +0000

    update to use grad accum

--- scripts/train_bi_ensemble.sh
+++ scripts/train_bi_ensemble.sh
@@ -32,7 +32,7 @@ CMD="python -m implicit.train_dot \
 --training_dataset_file $TRIPLE_FILE \
 --group_size $GROUP_SIZE \
 --per_device_train_batch_size $BASE_BATCH_SIZE \
---gradient_accumulation_steps 1 \
+--gradient_accumulation_steps 2 \
 --ir_dataset "msmarco-passage/train/triples-small" \
 --logging_steps 1000 \
 --save_steps 100000 \
