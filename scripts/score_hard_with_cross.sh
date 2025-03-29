609b8abd2e3b31a7c5d24e7c61850f7698fcbe51
“Andrew <a.parry.1@research.gla.ac.uk>
2025-03-29 16:59:32 +0000

    voila

--- scripts/score_hard_with_cross.sh
+++ scripts/score_hard_with_cross.sh
@@ -4,7 +4,7 @@ DATASET="msmarco-passage/train/triples-small"
 
 FILE=$OUTPUT_DIR/msmarco-hard-negatives.jsonl.gz
 
-python -m implicit.data_processing.get_scores \
+python -m implicit.data_processing.score_hard_negatives \
     --file $FILE \
     --model_name_or_path $CHECKPOINT \
     --dataset $DATASET \
