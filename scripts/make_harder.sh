67a595f83986f5c546194cf4c9bc9894744a620d
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-03 10:36:38 +0100

    why

--- scripts/make_harder.sh
+++ scripts/make_harder.sh
@@ -2,6 +2,6 @@ DATASET="msmarco-passage/train/triples-small"
 OUTPUT_DIR='data'
 
 # Mine negatives
-python sigir25-implicit-diff/src/implicit/data_processing/create_harder_negatives.py \
+python src/implicit/data_processing/create_harder_negatives.py \
     --triples_file "${OUTPUT_DIR}/triples.jsonl" \
     --data_folder $OUTPUT_DIR 
--- scripts/make_unfiltered.sh
+++ scripts/make_unfiltered.sh
@@ -2,6 +2,6 @@ DATASET="msmarco-passage/train/triples-small"
 OUTPUT_DIR='data'
 
 # Mine negatives
-python sigir25-implicit-diff/src/implicit/data_processing/create_harder_nofilter.py \
+python src/implicit/data_processing/create_harder_nofilter.py \
     --triples_file "${OUTPUT_DIR}/triples.jsonl" \
     --data_folder $OUTPUT_DIR 
