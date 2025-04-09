1ffddf9c4d029ffdf01c80b713f19a825d65a09b
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-09 14:30:32 +0200

    heheh

--- scripts/run_entropy.sh
+++ scripts/run_entropy.sh
@@ -4,7 +4,7 @@ DATASET="msmarco-passage/train/triples-small"
 
 FILE=$OUTPUT_DIR/$MODEL.16.jsonl
 
-MODELS=("bm25" "crossencoder" "ensemble" "random")
+MODELS=("bm25" "crossencoder" "random")
 
 for MODEL in ${MODELS[@]}; do
     python -m implicit.run_entropy \
@@ -14,3 +14,10 @@ for MODEL in ${MODELS[@]}; do
         --out_dir $OUTPUT_DIR \
         --name_override $MODEL
 done
+
+python -m implicit.run_entropy \
+    --file $OUTPUT_DIR/ensemble.16.jsonl \
+    --model_name_or_path $CHECKPOINT \
+    --dataset $DATASET \
+    --out_dir $OUTPUT_DIR \
+    --name_override ensemble.all
