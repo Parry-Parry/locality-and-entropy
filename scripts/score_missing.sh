28d0712e241cc066772d60a1b8642101ecb6e721
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-12 14:53:26 +0100

    should fix

--- scripts/score_missing.sh
+++ scripts/score_missing.sh
@@ -2,9 +2,11 @@ OUTPUT_DIR='data'
 CHECKPOINT='checkpoints/crossencoder'
 DATASET="msmarco-passage/train/triples-small"
 
-
+FILE=$1
+TEACHER_FILE=$2
 
 python -m implicit.data_processing.get_scores \
     --file $FILE \
+    --teacher_file $TEACHER_FILE \
     --model_name_or_path $CHECKPOINT \
     --dataset $DATASET \
