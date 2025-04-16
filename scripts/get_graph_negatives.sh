d2ff4555fb1c2c9992c466a0280d6fea9df98e09
“Andrew <a.parry.1@research.gla.ac.uk>
2025-04-16 10:05:06 +0100

    tidy up

--- scripts/get_graph_negatives.sh
+++
@@ -1,9 +0,0 @@
-OUTPUT_DIR='data'
-CHECKPOINT='checkpoints/crossencoder'
-DATASET="msmarco-passage/train/triples-small"
-
-# Mine negatives
-python src/implicit/data_processing/mine_negatives_from_graph.py \
-    --file "${OUTPUT_DIR}/triples.jsonl" \
-    --dataset $DATASET \
-    --out_dir $OUTPUT_DIR \
