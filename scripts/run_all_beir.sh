dcfe629aaca275b11608a3907cee13f5be724ed1
“Andrew <a.parry.1@research.gla.ac.uk>
2025-05-02 11:57:31 +0100

    voila

---
+++ scripts/run_all_beir.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MODEL_DIRECTORY="checkpoints"
+OUTPUT_DIRECTORY="beir_runs"
+BATCH_SIZE=512
+
+mkdir -p $OUTPUT_DIRECTORY
+
+CMD="python -m implicit.batch_run_beir \
+--ir_dataset $dataset \
+--model_directory $MODEL_DIRECTORY \
+--output_directory $OUTPUT_DIRECTORY"
+fi
+
+# Execute the command
+eval $CMD
