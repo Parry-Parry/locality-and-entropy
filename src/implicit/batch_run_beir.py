bbae6aae5d0d0dd843346b59775c9d50adf7b073
“Andrew <a.parry.1@research.gla.ac.uk>
2025-05-02 11:07:57 +0100

    add BEIR MAGIC

---
+++ src/implicit/batch_run_beir.py
@@ -0,0 +1,31 @@
+from .run_all_beir import run_topics
+import os
+from fire import Fire
+
+
+def batch_run_topics(
+    model_directory: str,
+    output_directory: str,
+    batch_size: int = 512,
+    dont_overwrite: bool = False,
+):
+    print(
+        f"Running with models in {model_directory} and saving to {output_directory}"
+    )
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory, exist_ok=True)
+    for model_name in os.listdir(model_directory):
+        model_path = os.path.join(model_directory, model_name)
+        run_topics(
+            model_path,
+            output_directory,
+            batch_size=batch_size,
+            dont_overwrite=dont_overwrite,
+            cat="dot" not in model_name,
+        )
+
+    return "Done!"
+
+
+if __name__ == "__main__":
+    Fire(batch_run_topics)
