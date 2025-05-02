883fab8d3ab90d9a135b2bda4825fa6396f30fb1
“Andrew <a.parry.1@research.gla.ac.uk>
2025-05-02 12:36:53 +0100

    its a mess

--- scripts/run_all_beir.sh
+++ scripts/run_all_beir.sh
@@ -7,10 +7,8 @@ BATCH_SIZE=512
 mkdir -p $OUTPUT_DIRECTORY
 
 CMD="python -m implicit.batch_run_beir \
---ir_dataset $dataset \
 --model_directory $MODEL_DIRECTORY \
 --output_directory $OUTPUT_DIRECTORY"
-fi
 
 # Execute the command
 eval $CMD
