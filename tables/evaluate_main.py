8d89a608e6dde8728d3f78d9f1a6ee5328e62dd2
“Andrew <a.parry.1@research.gla.ac.uk>
2025-05-09 09:38:34 +0100

    should fix

--- tables/evaluate_main.py
+++ tables/evaluate_main.py
@@ -128,6 +128,7 @@ def main(run_dir: str, out_dir: str, rel: int = 1, baseline: str = None):
             df_per['domain'] = df_per.name.map(lambda x: parse_run_meta(x)["domain"])
             df_per['loss']   = df_per.name.map(lambda x: parse_run_meta(x)["loss"])
             df_per['arch']   = df_per.name.map(lambda x: parse_run_meta(x)["arch"])
+            df_per['measure'] = df_per.measure.map(str)
             df_per['tmp_value'] = df_per['value']
             df_per.drop(columns=['value'], inplace=True)
             # ——— convert to long form ———
