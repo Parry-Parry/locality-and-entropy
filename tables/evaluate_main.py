7217266758a1aaf3025b306c559217e335304391
“Andrew <a.parry.1@research.gla.ac.uk>
2025-05-09 09:29:50 +0100

    hmm

--- tables/evaluate_main.py
+++ tables/evaluate_main.py
@@ -109,6 +109,7 @@ def main(run_dir: str, out_dir: str, rel: int = 1, baseline: str = None):
             df_per['arch']   = df_per.name.map(lambda x: parse_run_meta(x)["arch"])
             df_per['tmp_value'] = df_per['value']
             df_per.drop(columns=['value'], inplace=True)
+            print(df_per['measure'].unique())
             # ——— convert to long form ———
             df_per = df_per.melt(
                 id_vars=['name','qid','domain','loss','arch'],
